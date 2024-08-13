970cb2724fbdc0e384ea33b4d028685fbdfa0991
Vishal-Mutha <145339077+Vishal-Mutha@users.noreply.github.com>
2024-08-13 19:11:07 +0530

    Add files via upload

---
+++ IR_+_MotorDriver/components/include/ir.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include "driver/gpio.h"
+
+#define IR_LEFTMOST GPIO_NUM_17
+#define IR_RIGHTMOST GPIO_NUM_15
+#define IR_CENTER GPIO_NUM_4
+
+void IR_Enable();
+
