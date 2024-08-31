324542837d835fbf2087262c6e4f96e318402edc
Vishal-Mutha <145339077+Vishal-Mutha@users.noreply.github.com>
2024-09-01 02:23:28 +0530

    Delete firmware/Left_follow_rule directory

--- firmware/Left_follow_rule/components/ir.c
+++
@@ -1,8 +0,0 @@
-#include "ir.h"
-#include <stdio.h>
-
-void IR_Enable() {
-    gpio_set_direction(IR_LEFTMOST, GPIO_MODE_INPUT);
-    gpio_set_direction(IR_RIGHTMOST, GPIO_MODE_INPUT);
-    gpio_set_direction(IR_CENTER, GPIO_MODE_INPUT);
-}
